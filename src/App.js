beac2422a15b35bb67664b7ebf68f76f6381a201
Karol-Waliszewski <karolwaliszewski@gmail.com>
2019-01-02 23:27:45 +0100

    Photo slider beta is ready

--- src/App.js
+++ src/App.js
@@ -2,32 +2,37 @@ import React, { Component, Fragment } from "react";
 import PhotoSlider from "./react-photo-slider";
 
 class App extends Component {
+  state = { activePhoto: 0 };
+
+  setActive(index) {
+    this.setState({ activePhoto: index });
+  }
+
   render() {
+    let buttons = [1, 2, 3].map((data, index) => (
+      <button
+        className="slider__dot"
+        onClick={this.setActive.bind(this, index)}
+      >
+        {index + 1}
+      </button>
+    ));
     return (
       <Fragment>
         <section class="hero is-info is-fullheight">
           <div class="hero-body" style={{ height: "100vh" }}>
             <PhotoSlider
+              style={{ border: "4px solid #000" }}
               photos={[
                 "https://images.pexels.com/photos/1168742/pexels-photo-1168742.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
                 "https://images.pexels.com/photos/691571/pexels-photo-691571.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
                 "https://images.pexels.com/photos/457044/pexels-photo-457044.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
               ]}
+              activePhoto={this.state.activePhoto}
             />
           </div>
 
-          {/* <div class="hero-body">
-            <div class="container has-text-centered">
-              <div class="column is-6 is-offset-3">
-                <h1 class="title">Coming Soon</h1>
-                <h2 class="subtitle">
-                  $this is the best software platform for running an internet
-                  business. We handle billions of dollars every year for
-                  forward-thinking businesses around the world.
-                </h2>
-              </div>
-            </div>
-          </div> */}
+          {buttons}
         </section>
       </Fragment>
     );
--- src/react-photo-slider.js
+++ src/react-photo-slider.js
@@ -1,6 +1,7 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 
 import "./react-photo-slider.css";
+import Arrow from "./arrow.svg";
 
 class photoSlider extends Component {
   constructor() {
@@ -36,34 +37,33 @@ class photoSlider extends Component {
     });
   }
 
+  componentWillReceiveProps(newProps) {
+    if (typeof newProps.activePhoto != "undefined") {
+      this.choosePhoto(newProps.activePhoto);
+    }
+  }
+
   render() {
     let photos = this.props.photos.map((data, index) => (
       <img src={data} className="slider__image" key={index} />
     ));
 
-    let buttons = this.props.photos.map((data, index) => (
-      <button
-        className="slider__dot"
-        onClick={this.choosePhoto.bind(this, index)}
-      >
-        {index + 1}
-      </button>
-    ));
-
     return (
-      <div className="slider__container">
+      <div className="slider__container" style={this.props.style}>
         <div className="slider__wrapper" ref={this.wrapper}>
           <div className="slider__images">{photos}</div>
         </div>
         <button
-          className="slider__button"
+          className="slider__button--left"
           onClick={this.previousPhoto.bind(this)}
         >
-          previous
+          <img src={Arrow} alt="arrow pointing to the left" />
         </button>
-        {buttons}
-        <button className="slider__button" onClick={this.nextPhoto.bind(this)}>
-          next
+        <button
+          className="slider__button--right"
+          onClick={this.nextPhoto.bind(this)}
+        >
+          <img src={Arrow} alt="arrow pointing to the right" />
         </button>
       </div>
     );
